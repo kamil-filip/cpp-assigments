9d5ec44d35a7f720699c5bc6121fceccd6021d54
kamil-filip <kamilfkozlowski@gmail.com>
2016-06-01 09:06:08 +0200

    from yesterday

---
+++ Assigment1/ex_5/main.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+#define MONDAY4 15
+
+
+
+enum EWDays
+{
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNSEDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+};
+
+enum class ESDays
+{
+	SUNDAY,
+	MONDAY = 1,
+	TUESDAY,
+	WEDNSEDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDA2
+};
+
+
+int main()
+{
+	EWDays monday0 = MONDAY;
+	EWDays monday1 = MONDAY;
+	ESDays monday01 = ESDays::MONDAY;
+	ESDays monday11 = ESDays::MONDAY;
+
+
+
+	if(monday0 == 1)
+	{
+		cout<<"Are the same \n";
+	}
+
+	if(monday01 == monday11)
+	{
+		cout<<"Are the same \n";
+	}
+
+
+
+	return 0;
+}
---
+++ Assigment1/ex_8/main.cc
@@ -0,0 +1,20 @@
+// Railroad Companies
+#define NS      0
+#define DB      1
+#define SNCF    2
+#define BR      3
+#define AMTRAK  4
+
+//Airline Companies
+#define BritishAirways  0
+#define KLM             1
+#define Lufthansa       2
+#define UnitedAirlines  3
+
+void airlineSchedule(int company);
+void railroadSchedule(int company);
+
+int main()
+{
+    // calls the various schedule functions declared above
+}
